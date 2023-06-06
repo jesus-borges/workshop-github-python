fd5e87c86eef59af48c72833baa650d9eb0c25ad
jesus-borges <jesus.borges2882@gmail.com>
2023-06-06 20:53:48 -0300

    inica o programa

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input('digite seu nome: ')
+city = input('digite a cidade: ')
+gender = input('digite o sexo: ')
+               
+
+print('seus dados sao: ')
+print(name, ' - ', city, ' - ', gender)
