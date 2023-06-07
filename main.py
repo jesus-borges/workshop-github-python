a741295603f0329d4cc367a20da01c4d572d7301
jesus-borges <jesus.borges2882@gmail.com>
2023-06-06 22:07:28 -0300

    cria variavel idade

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ city = input('digite a cidade: ')
 gender = input('digite o sexo: ')
 age = input ('digite idade: ')            
 
-print('seguem os dados: ')
+print('seguem os dado: ')
 print(name, ' - ', city, ' - ', gender)
